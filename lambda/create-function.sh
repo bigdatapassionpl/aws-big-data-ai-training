04eafa752fb4ad364cc99e6627a2c7431e2b828f
Radosław Szmit <radoslawszmit@gmail.com>
2019-03-13 21:08:18 +0100

    AWS Lambda

--- lambda/create-function.sh
+++ lambda/create-function.sh
@@ -4,7 +4,7 @@ aws lambda create-function \
     --function-name lambda-function-in-java \
     --runtime java8 \
     --role arn:aws:iam::252594130684:role/Politechnika  \
-    --handler example.Hello \
+    --handler com.bigdatapassion.SimpleLambda \
     --timeout 15 \
     --memory-size 512 \
     --zip-file fileb://target/lambda-1.0-SNAPSHOT.jar
