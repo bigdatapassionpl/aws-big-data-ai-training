e0b9668c9f367f14c007db6cf6e3b59eceff34ba
Radosław Szmit <radoslawszmit@gmail.com>
2019-03-14 09:50:48 +0100

    AWS Lambda

--- lambda/script/create-function-java.sh
+++ lambda/script/create-function-java.sh
@@ -4,10 +4,37 @@ cd ..
 mvn clean install
 
 aws lambda create-function \
-    --function-name lambda-function-in-java \
+    --function-name lambda-function-in-java-dynamodb \
     --runtime java8 \
     --role arn:aws:iam::252594130684:role/Politechnika  \
     --handler com.bigdatapassion.DynamoDBLambda \
+    --timeout 15 \
+    --memory-size 512 \
+    --zip-file fileb://target/lambda-1.0-SNAPSHOT.jar
+
+aws lambda create-function \
+    --function-name lambda-function-in-java-hello \
+    --runtime java8 \
+    --role arn:aws:iam::252594130684:role/Politechnika  \
+    --handler com.bigdatapassion.HelloLambda \
+    --timeout 15 \
+    --memory-size 512 \
+    --zip-file fileb://target/lambda-1.0-SNAPSHOT.jar
+
+aws lambda create-function \
+    --function-name lambda-function-in-java-simple \
+    --runtime java8 \
+    --role arn:aws:iam::252594130684:role/Politechnika  \
+    --handler com.bigdatapassion.SimpleLambda \
+    --timeout 15 \
+    --memory-size 512 \
+    --zip-file fileb://target/lambda-1.0-SNAPSHOT.jar
+
+aws lambda create-function \
+    --function-name lambda-function-in-java-stream \
+    --runtime java8 \
+    --role arn:aws:iam::252594130684:role/Politechnika  \
+    --handler com.bigdatapassion.StreamLambda \
     --timeout 15 \
     --memory-size 512 \
     --zip-file fileb://target/lambda-1.0-SNAPSHOT.jar
--- lambda/script/delete-function-java.sh
+++ lambda/script/delete-function-java.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-aws lambda delete-function \
-    --function-name lambda-function-in-java
+aws lambda delete-function --function-name lambda-function-in-java-simple
+aws lambda delete-function --function-name lambda-function-in-java-dynamodb
+aws lambda delete-function --function-name lambda-function-in-java-hello
+aws lambda delete-function --function-name lambda-function-in-java-stream
