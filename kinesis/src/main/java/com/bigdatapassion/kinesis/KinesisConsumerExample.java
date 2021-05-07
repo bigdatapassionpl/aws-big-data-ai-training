acba8642dce21d0b9d8270730fcb520e54eb3f9e
Radosław Szmit <radoslawszmit@gmail.com>
2021-05-07 13:34:20 +0200

    AWS Kinesis prod con example

--- kinesis/src/main/java/com/bigdatapassion/kinesis/KinesisConsumerExample.java
+++ kinesis/src/main/java/com/bigdatapassion/kinesis/KinesisConsumerExample.java
@@ -5,6 +5,8 @@ import software.amazon.awssdk.services.kinesis.model.*;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class KinesisConsumerExample {
 
@@ -25,34 +27,55 @@ public class KinesisConsumerExample {
         List<Shard> shards = describeResponse.streamDescription().shards();
 
         // https://docs.aws.amazon.com/kinesis/latest/APIReference/API_StartingPosition.html
-        Shard shard = shards.get(0);
+
+        Map<String, String> shardsIterators = shards.stream()
+                .collect(Collectors.toMap(
+                        Shard::shardId,
+                        shard -> getFirstIteratorForShard(kinesisConfiguration, kinesisClient, shard)
+                ));
+
+
+        while (true) {
+
+            shardsIterators = shardsIterators.entrySet().stream()
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            entry -> processNextShardMessages(kinesisClient, entry.getKey(), entry.getValue())
+                    ));
+
+            // waiting for next get records due to limitations
+            Thread.sleep(1000);
+        }
+
+    }
+
+    private static String getFirstIteratorForShard(KinesisConfiguration kinesisConfiguration, KinesisClient kinesisClient, Shard shard) {
+
         GetShardIteratorRequest iteratorRequest = GetShardIteratorRequest.builder()
                 .streamName(kinesisConfiguration.getStreamName())
                 .shardId(shard.shardId())
                 .shardIteratorType(ShardIteratorType.TRIM_HORIZON)
                 .build();
 
-        GetShardIteratorResponse iteratorResponse = kinesisClient.getShardIterator(iteratorRequest);
-        String shardIterator = iteratorResponse.shardIterator();
+        return kinesisClient.getShardIterator(iteratorRequest).shardIterator();
+    }
 
-        while (true) {
-            System.out.println("Interator: " + shardIterator);
+    /**
+     * as simple as possible, display only
+     */
+    private static String processNextShardMessages(KinesisClient kinesisClient, String shardId, String shardIterator) {
 
-            GetRecordsRequest kinesisRequest = GetRecordsRequest.builder()
-                    .shardIterator(shardIterator)
-                    .build();
+        GetRecordsRequest kinesisRequest = GetRecordsRequest.builder()
+                .shardIterator(shardIterator)
+                .build();
 
-            GetRecordsResponse recordsResponse = kinesisClient.getRecords(kinesisRequest);
+        GetRecordsResponse recordsResponse = kinesisClient.getRecords(kinesisRequest);
 
-            if (recordsResponse.hasRecords()) {
-                recordsResponse.records().forEach(record -> System.out.println(record.data().asString(StandardCharsets.UTF_8)));
-            }
-
-            shardIterator = recordsResponse.nextShardIterator();
-
-            Thread.sleep(1000);
+        if (recordsResponse.hasRecords()) {
+            recordsResponse.records().forEach(record -> System.out.println(shardId + " -> " + record.data().asString(StandardCharsets.UTF_8)));
         }
 
+        return recordsResponse.nextShardIterator();
     }
 
 }
