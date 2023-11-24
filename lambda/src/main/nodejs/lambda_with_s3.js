7fc7e5d16d2510be5d9a46b67cb2fc9352e2b4c7
Radosław Szmit <radoslawszmit@gmail.com>
2023-11-24 19:05:32 +0100

    Simple Lambda Node.js 20

--- lambda/src/main/nodejs/lambda_with_s3.js
+++ lambda/src/main/nodejs/lambda_with_s3.js
@@ -1,23 +1,51 @@
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-exports.handler = function (event, context, callback) {
+const  { AWS } = require ( "@aws-sdk/client-s3" );
 
-    // S3 params (bucket and object key)
-    var src_bkt = 'aws-educate-radek-test';
-    var src_key = 'test.txt';
+export const handler = async (event, context) => {
+    // Set up the S3 client
+    const s3 = new AWS.S3();
 
-    // Get the object
-    s3.getObject({
-        Bucket: src_bkt,
-        Key: src_key
-    }, function (err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            callback(err);
-        } else {
-            console.log('\n\n' + data.Body.toString() + '\n');
-            callback(null, data.Body.toString());
-        }
-    });
+    // Specify the bucket and file key
+    const bucketName = 'sdgagqwagqwae';
+    const fileKey = 'test.txt';
+
+    try {
+        // Get the file from S3
+        const params = {
+            Bucket: bucketName,
+            Key: fileKey
+        };
+
+        const s3Object = await s3.getObject(params).promise();
+
+        // Process the file content (you can customize this part based on your needs)
+        const fileContent = s3Object.Body.toString('utf-8');
+
+        // Log or process the file content as needed
+        console.log('File Content:', fileContent);
+
+        // You can also return the file content in the response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                fileContent: fileContent
+            }),
+        };
+
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+
+        // Return an error response
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: 'Failed to retrieve the file from S3'
+            }),
+        };
+
+        return response;
+    }
 };
